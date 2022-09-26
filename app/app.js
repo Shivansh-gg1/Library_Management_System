8e9a07339a16fa4309da30842cb35bb0f48b4a89
Shivansh-gg1 <shivanshsharma.ait@gmail.com>
2022-09-26 18:41:52 +0530

    80 percent donn

---
+++ app/app.js
@@ -0,0 +1,19 @@
+const express = require('express'),             // Required for creating server
+    bodyParser = require('body-parser'),       // Parses body of URL
+    cors = require('cors');                   // For security features
+
+
+//Using Dependencies that we imported previously
+const app = express();                  // using express for creating server
+app.use(cors('*'));                    // using Cors for creatig security features
+app.use(bodyParser.json());           // using bodyParser for parsing the body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Linking Routes File
+app.use('/book', require('../app/Routes/book_router'));
+app.use('/issuedate', require('../app/Routes/bookIssue_route'));
+// app.use('/student',require('../app/Routes/student_route'));
+// app.use('/librarian',require('../app/Routes/librarian_route'));
+
+//Exporting module of app
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();    //Configured DotEnv File
+
+const mongoose = require('mongoose');
+const app = require('./app/app');           //Importing App.js
+
+//Listening on PORT
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server Started on Port: ${PORT}`);
+});
+
+
+//Database Connectivity
+const DB_URL = process.env.DB_URL;
+mongoose.connect(DB_URL).then(() => console.log(`Database Connected Successfully`))
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
